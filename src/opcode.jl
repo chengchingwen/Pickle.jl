76d38e6d42734929d509ed24eddd70251d7d152e
chengchingwen <adgjl5645@hotmail.com>
2020-04-05 03:22:19 +0800

    genops sketch

--- src/opcode.jl
+++ src/opcode.jl
@@ -1,19 +1,21 @@
-@enum OpCode begin
+using Base.Enums: namemap
+
+@enum OpCode::UInt8 begin
     # integers
-    int                = Int('I')
-    binint             = Int('J')
-    binint1            = Int('K')
-    binint2            = Int('M')
-    long               = Int('L')
+    int                = codepoint('I')
+    binint             = codepoint('J')
+    binint1            = codepoint('K')
+    binint2            = codepoint('M')
+    long               = codepoint('L')
     long1              = 0x8a
     long4              = 0x8b
     # strings
-    string             = Int('S')
-    binstring          = Int('T')
-    short_binstring    = Int('U')
+    string             = codepoint('S')
+    binstring          = codepoint('T')
+    short_binstring    = codepoint('U')
     # bytes (protocal 3 and higher)
-    binbytes           = Int('B')
-    short_binbytes     = Int('C')
+    binbytes           = codepoint('B')
+    short_binbytes     = codepoint('C')
     binbytes8          = 0x8e
     # bytearray (protocal 5 and higher)
     bytearray8         = 0x96
@@ -21,72 +23,103 @@
     next_buffer        = 0x97
     readonly_buffer    = 0x98
     # none
-    none               = Int('N')
+    none               = codepoint('N')
     # bool (protocal 2 and higher)
     newtrue            = 0x88
     newfalse           = 0x89
     # unicode string
-    unicode            = Int('V')
+    unicode            = codepoint('V')
     short_binunicode   = 0x8c
-    binunicode         = Int('X')
+    binunicode         = codepoint('X')
     binunicode8        = 0x8d
     # floats
-    float              = Int('F')
-    binfloat           = Int('G')
+    float              = codepoint('F')
+    binfloat           = codepoint('G')
     # build list
-    empty_list         = Int(']')
-    append             = Int('a')
-    appends            = Int('e')
-    list               = Int('l')
+    empty_list         = codepoint(']')
+    append             = codepoint('a')
+    appends            = codepoint('e')
+    list               = codepoint('l')
     # build tuples
-    empty_tuple        = Int(')')
-    tuple              = Int('t')
+    empty_tuple        = codepoint(')')
+    tuple              = codepoint('t')
     tuple1             = 0x85
     tuple2             = 0x86
     tuple3             = 0x87
     # build dicts
-    empty_dict         = Int('}')
-    dict               = Int('d')
-    setitem            = Int('s')
-    setitems           = Int('u')
+    empty_dict         = codepoint('}')
+    dict               = codepoint('d')
+    setitem            = codepoint('s')
+    setitems           = codepoint('u')
     # build sets
     empty_set          = 0x8f
     additems           = 0x90
     # build frozensets
     frozenset          = 0x91
     # stack manipulation
-    pop                = Int('0')
-    dup                = Int('2')
-    mark               = Int('(')
-    pop_mark           = Int('1')
+    pop                = codepoint('0')
+    dup                = codepoint('2')
+    mark               = codepoint('(')
+    pop_mark           = codepoint('1')
     # memo manipulation
-    get                = Int('g')
-    binget             = Int('h')
-    long_binget        = Int('j')
-    put                = Int('p')
-    binput             = Int('q')
-    long_binput        = Int('r')
+    get                = codepoint('g')
+    binget             = codepoint('h')
+    long_binget        = codepoint('j')
+    put                = codepoint('p')
+    binput             = codepoint('q')
+    long_binput        = codepoint('r')
     memoize            = 0x94
     # extension registry
     ext1               = 0x82
     ext2               = 0x83
     ext4               = 0x84
     # push class or function to stack by module and name
-    Global             = Int('c')
+    Global             = codepoint('c')
     stack_Global       = 0x93
     # build unknown objects
-    reduce             = Int('R')
-    build              = Int('b')
-    inst               = Int('i')
-    obj                = Int('o')
+    reduce             = codepoint('R')
+    build              = codepoint('b')
+    inst               = codepoint('i')
+    obj                = codepoint('o')
     newobj             = 0x81
     newobj_ex          = 0x92
     # machine control
     proto              = 0x80
-    stop               = Int('.')
+    stop               = codepoint('.')
     # Framing support
     frame              = 0x95
     # persistent IDs
-    persid             = Int('P')
-    binpersid          = Int('Q')
+    persid             = codepoint('P')
+    binpersid          = codepoint('Q')
+end
+
+function maybe_opcode(x)
+    if x in keys(namemap(OpCode))
+        return OpCode(x)
+    end
+    return nothing
+end
+
+if VERSION < v"1.1"
+    isnothing(::Nothing) = true
+    isnothing(::Any) = false
+end
+
+function genops(io::IO, yield_end_pos=false)
+    Channel() do chn
+        while !eof(io)
+            pos = position(io)
+            code = read(io, 1)[]
+            opcode = maybe_opcode(code)
+            isnothing(opcode) &&
+                error("at position $pos, code $code unknown.")
+
+            arg = arguement(opcode)
+            if yield_end_pos
+                put!(chn, (opcode, arg, pos, position(io)))
+            else
+                put!(chn, (opcode, arg, pos))
+            end
+        end
+    end
 end

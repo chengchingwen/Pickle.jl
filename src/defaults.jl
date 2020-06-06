546f46f9311028bd1548b17a7bba0c3494c12d37
chengchingwen <adgjl5645@hotmail.com>
2020-06-06 16:31:47 +0800

    module alias for old python

--- src/defaults.jl
+++ src/defaults.jl
@@ -2,6 +2,8 @@ function default_methods!(mt)
   mt["builtins.set"] = Set
   mt["builtins.bytes"] = () -> b""
   mt["codecs.encode"] = (s, c) -> codeunits(s)
+  _setentry!(mt.head, mt["builtins"], "__builtin__")
+  _setentry!(mt.head, mt["codecs"], "_codecs")
   mt["__julia__.Set"] = "builtins.set"
   mt["__julia__.Base.CodeUnits"] = "codecs.encode"
   mt["__julia__.__py__.bytes"] = "builtins.bytes"

ac6324816811d60175ae15c71d371e65afa48659
chengchingwen <adgjl5645@hotmail.com>
2020-06-06 14:12:20 +0800

    add default methods for bytes

--- src/defaults.jl
+++ src/defaults.jl
@@ -1,5 +1,7 @@
 function default_methods!(mt)
   mt["builtins.set"] = Set
+  mt["builtins.bytes"] = () -> b""
+  mt["codecs.encode"] = (s, c) -> codeunits(s)
   mt["__julia__.Set"] = "builtins.set"
   mt["__julia__.Base.CodeUnits"] = "codecs.encode"
   mt["__julia__.__py__.bytes"] = "builtins.bytes"

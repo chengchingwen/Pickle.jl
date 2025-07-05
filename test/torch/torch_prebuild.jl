8137c7d07c45024050ece264babc3c334f9efd1f
chengchingwen <chengchingwen214@gmail.com>
2025-07-05 21:19:47 +0800

    fix test

--- test/torch/torch_prebuild.jl
+++ test/torch/torch_prebuild.jl
@@ -1,11 +1,13 @@
 @testset "PREBUILD" begin
 
-  for f in readdir(joinpath(@__DIR__, "test_bin"))
-    file = joinpath(@__DIR__, "test_bin", f)
-    x1,   x2 = Pickle.Torch.THload(file)
-    px1, px2 = pyconvert(Dict{String, Py}, libtorch.load(file))
-    @test x1[2] ≈ pyconvert(Any, px1[2].numpy())
-    @test x2[2] ≈ pyconvert(Any, px2[2].numpy())
-  end
+    for f in readdir(joinpath(@__DIR__, "test_bin"))
+        file = joinpath(@__DIR__, "test_bin", f)
+        jlpkl = Pickle.Torch.THload(file)
+        pypkl = pyconvert(Dict{String, Py}, libtorch.load(file))
+        @test keys(jlpkl) == keys(pypkl)
+        for k in keys(jlpkl)
+            @test jlpkl[k] ≈ pyconvert(Any, pypkl[k].numpy())
+        end
+    end
 
 end
